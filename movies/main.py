bc0cbbca93b6562d785a7a13ecc44df8a75e7ee9
Alexey Solod <charmy.machine@yandex.ru>
2025-05-04 19:29:37 +0300

    create view for root path

--- movies/main.py
+++ movies/main.py
@@ -1,3 +1,16 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
-app = FastAPI()
+app = FastAPI(title="Movies catalog")
+
+
+@app.get("/")
+def read_root(request: Request):
+    docs_url = request.url.replace(path="/docs")
+
+    return {
+        "message": "Hello World",
+        "docs": str(docs_url),
+    }
