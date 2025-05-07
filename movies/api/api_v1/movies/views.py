35864f89e5404d7c156a307ddc5e7aa10e5171a2
Alexey Solod <charmy.machine@yandex.ru>
2025-05-07 13:48:52 +0300

    create path for creating new Movie

--- movies/api/api_v1/movies/views.py
+++ movies/api/api_v1/movies/views.py
@@ -1,6 +1,13 @@
+import random
 from typing import Annotated
+from annotated_types import Len
 
-from fastapi import Depends, APIRouter
+from fastapi import (
+    Depends,
+    APIRouter,
+    Form,
+    status,
+)
 
 from api.api_v1.movies.crud import MOVIE_ITEMS
 from api.api_v1.movies.dependencies import prefetch_movie
@@ -20,3 +27,20 @@ def get_movies_list():
 @router.get("/{movie_id}", response_model=Movie)
 def get_movie_details(movie_detail: Annotated[Movie, Depends(prefetch_movie)]):
     return movie_detail
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    title: Annotated[str, Len(min_length=3, max_length=100), Form()],
+    description: Annotated[str, Len(min_length=3, max_length=100), Form()],
+):
+    return Movie(
+        movie_id=random.randint(1, 100),
+        title=title,
+        description=description,
+        rating=round(random.uniform(8.0, 10.0), 2),
+    )
